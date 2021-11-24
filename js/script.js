08700e6427a36445dad2ecba44744af0e47456a7
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2021-11-24 14:59:47 +0100

    completato esercizio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,47 @@
 
-//
+// Funzione che ritorna numeri casuali
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min); 
   }
   
+// Creazione array dei numeri del gioco
+const arrayNumber = [];
+
+while(arrayNumber.length < 5){
+    let numb = randomNumber(1,100);
+    if(!arrayNumber.includes(numb)){
+        arrayNumber.push(numb);
+    }
+}
+
+
+setTimeout(controlNumber, 10000);
+    
+const boxNumber = document.querySelector(".boxNumber");
+    boxNumber.innerText = arrayNumber;  
+
+
+// Funzione per il controllo dei numeri memorizzati
+function controlNumber(){
+    boxNumber.innerText = ''; 
+
+    const numbRemembered = [];
+
+    for(let i = 0; i < 5; i++){
+        let numb = parseInt(prompt('Inserisci un numero'));
+        if(arrayNumber.includes(numb)){
+            numbRemembered.push(numb);
+        }
+        i;
+    }
+
+    if (numbRemembered.length < 5 ){
+        boxNumber.innerText = numbRemembered;
+        const boxTitle = document.querySelector(".boxTitle");
+        boxTitle.innerText = "I numeri che hai ricordato sono";
+    }  else{
+        boxNumber.innerText = "Hai  vinto !";
+    }
+    
+}
+
